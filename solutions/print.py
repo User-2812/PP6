dfc2f78f6d33ecf8a62f9fc3670a0bd39d27ea44
User-2812 <wrobla28@stud.thga.de>
2025-05-26 19:51:34 +0200

    print.py

---
+++ solutions/print.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def main():
+    print("Hello dies ist eine Python ausgabe von print.py.")
+    print("A", "B", "C", sep="-", end=".\n")
+    value = 2 + 2
+    print(f"Two plus two equals {value}")
+
+if __name__ == "__main__":
+    main()
