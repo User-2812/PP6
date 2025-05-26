62b54e34495c666bdbb4ff3536efc930cbf7e709
User-2812 <wrobla28@stud.thga.de>
2025-05-26 19:30:16 +0200

    print.c

---
+++ solutions/print.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello User-2812!\n");
+    printf("Integer: %d, Float: %.2f, String: %s\n", 2812, 3.14, "Works");
+    puts("This is puts output");
+    return 0;
+}
